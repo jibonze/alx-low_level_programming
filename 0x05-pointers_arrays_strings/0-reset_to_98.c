9660db34a927a2e9431bfe2bd66b49b26ee3cf65
jibonze <jibonze63@gmail.com>
2024-07-06 15:21:49 -0700

    0-reset_to-98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-void updateValue(int *p) {
-    *p = 98;
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98.
+ * @n: Pointer to an integer.
+ */
+void reset_to_98(int *n)
+{
+    if (n != NULL)  // Check if the pointer is valid
+        *n = 98;    // Update the value pointed to by n
 }
 
-int main() {
-    int n = 42;
-    printf("Before update: %d\n", n);
-    updateValue(&n);
-    printf("After update: %d\n", n);
+int main()
+{
+    int num = 402;
+    printf("n=%d\n", num);
+    reset_to_98(&num);
+    printf("n=%d\n", num);
     return 0;
 }
