b17d4f5ed9bbe2a4a5bec3ba7aa51c1fd2d59ac4
jibonze <jibonze63@gmail.com>
2024-07-07 14:14:01 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,30 +6,25 @@
  */
 void times_table(void)
 {
-    int row, column, product;
-
-    for (row = 0; row <= 9; row++)
+  int row, col, product;
+  for (row = 0; row <= 9; row++)
     {
-        for (column = 0; column <= 9; column++)
+      for (col = 0; col <= 9; col++)
         {
-            product = row * column;
-            if (column == 0)
-                _putchar('0');
-                _putchar(' ');
+	  product = row * col;
+	  if (col == 0)
+	    _putchar('0');
+	  else
+	    _putchar(',');
+	  _putchar(' ');
 
-            if (product < 10)
-                _putchar(' ');
-            else
-                _putchar((product / 10) + '0');
-            _putchar((product % 10) + '0');
+	  if (product < 10)
+	    _putchar(' ');
+	  else
+	    _putchar((product / 10) + '0');
 
-            if (column < 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            else
-                _putchar('\n'); 
+	  _putchar((product % 10) + '0');
         }
+      _putchar('\n');
     }
 }
