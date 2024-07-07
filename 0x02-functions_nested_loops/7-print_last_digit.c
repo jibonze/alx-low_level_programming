5e0597f2187a17767dde39d6e1e8ca4689fb78c6
jibonze <jibonze63@gmail.com>
2024-07-07 13:49:45 -0700

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,9 +5,3 @@ int print_last_digit(int num) {
   printf("The last digit is: %d\n", last_digit);
   return last_digit;
 }
-
-int main() {
-  int number = 12345;
-  print_last_digit(number);
-  return 0;
-}
